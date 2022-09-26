d5c230933486c0df5858aea90730823b3e540391
adeelikram <rahil.ikram67@gmail.com>
2022-09-26 11:14:30 +0500

    L

--- src/event/grab.ts
+++ src/event/grab.ts
@@ -5,15 +5,13 @@ export async function grab(page: play.Page, url: string): Promise<Post | undefin
     if (!page) return
     await new Promise(resolve => setTimeout(resolve, random(1, 5) * 1000))
     page.on("request", (r) => redirectHandler(r, page))
-    page.on("console",(message)=>{
-        console.log(message.text())
-    })
+    
     try {
-        await page.goto(url,{waitUntil:"networkidle"})
-        await page.locator("article > div > div > div > div:nth-child(1) > a > div > div._aagv > img").first().waitFor({ state: "attached", timeout: 15000 })
-        await page.locator("span._aacl").first().waitFor({ state: "attached", timeout: 15000 })
-        await page.locator("article > div > div > div > div:nth-child(1) > a").first().waitFor({ state: "attached", timeout: 15000 })
-        await page.locator("span > img").first().waitFor({ state: "attached", timeout: 15000 })
+        await page.goto(url)
+        await page.locator("article > div > div > div > div:nth-child(1) > a > div > div._aagv > img").first().waitFor({ state: "attached", timeout: 0 })
+        await page.locator("span._aacl").first().waitFor({ state: "attached", timeout: 0 })
+        await page.locator("article > div > div > div > div:nth-child(1) > a").first().waitFor({ state: "attached", timeout: 0 })
+        await page.locator("span > img").first().waitFor({ state: "attached", timeout: 0 })
         const ref = await page.$$("article > div > div > div > div:nth-child(1) > a > div > div._aagv > img")
         const post_tag = await page.$$("article > div > div > div > div:nth-child(1) > a")
         const profile_img = await page.$$("span > img")
