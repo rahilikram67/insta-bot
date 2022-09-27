fbcdf06b6d2db4d9795d935055e1db2786fb7ca9
adeelikram <rahil.ikram67@gmail.com>
2022-09-27 21:01:22 +0500

    M

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -7,9 +7,9 @@ export async function logger(config: Config & RestProps) {
     try {
         await page.goto("https://www.instagram.com/accounts/login/", { waitUntil: "networkidle" })
         await page.locator('[name=username]').click();
-        await page.locator('[name=username]').fill(process.env.LOGIN || "",);
+        await page.locator('[name=username]').type(process.env.LOGIN || "", { delay: 60 });
         await page.locator('[aria-label="Password"]').click();
-        await page.locator('[aria-label="Password"]').fill(process.env.PASSWORD || "");
+        await page.locator('[aria-label="Password"]').type(process.env.PASSWORD || "", { delay: 60 });
         await page.locator('button:has-text("Log In")').first().click();
         await page.waitForNavigation({ waitUntil: "networkidle" })
         config.login = true
